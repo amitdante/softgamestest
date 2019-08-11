3426d9f4cd9070087ced27398a97b8d6fea2d26f
amitjunglee <amit.dhillon@jungleegames.com>
2019-08-11 19:26:54 +0530

    Added clean webpack plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,12 @@
 const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
     mode: "production",
     output: {
-        filename: 'bundle[contentHash].js',
+        filename: '[name][contentHash].js',
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [new CleanWebpackPlugin()]
